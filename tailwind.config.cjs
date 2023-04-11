2713a0fa57a57d4f61b4016b37605d501839a373
Bruno <brunohdzv@gmail.com>
2023-04-11 13:02:48 -0600

    #Codigo faltante de tailwind para tipografías
    #Bug error de tipografia, no reconocida en browser

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily:{
-				sans:['"TT Norms"', 'sans-serif']
+				sans:['"TT Norms"', 'sans-serif', ...defaultTheme.fontFamily.sans]
 			},
 			colors:{
 				ghost: '#F7F7FF',
